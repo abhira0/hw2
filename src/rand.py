314e3edef44b9cc8bc4d59f66f74acd8bd868c19
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-09-01 02:06:45 +0000

    changes as per pylint

--- src/rand.py
+++ src/rand.py
@@ -1,10 +1,12 @@
+"""Module containing code to generate random array."""
 import subprocess
 
 
 def random_array(arr):
+    """Function that returns random elements in an array by using 'shuf' cli tool."""
     shuffled_num = None
-    for i in range(len(arr)):
+    for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)
     return arr
