3ce666dd7933347a2cc4da4f1e69cb9594f8280e
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-09-01 17:33:46 +0000

    remoev failing test

--- tests/test_hw2_debugging.py
+++ tests/test_hw2_debugging.py
@@ -51,7 +51,3 @@ def test_only_negatives_array():
     arr = [-i for i in range(1, 11)]
     merge_sorted_array = merge_sort(arr)
     assert merge_sorted_array == sorted(arr), "Array is not sorted"
-
-
-def test_fail():
-    assert False, "FAILING"
