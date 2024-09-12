6fd53b9b64d378dc84674dbfce71730f8cba818a
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-09-12 05:12:20 +0000

    test changes

--- tests/test_hw2_debugging.py
+++ tests/test_hw2_debugging.py
@@ -1,19 +1,39 @@
-import pytest
+"""testing merge sort."""
 from src.hw2_debugging import merge_sort
+from src.rand import random_array
 
 
 def test_merge_sort_empty():
-    assert merge_sort([]) == []
+    """Array here is empty."""
+    arr = []
+    assert merge_sort(arr) == sorted(arr), "Array is not sorted"
 
 
 def test_merge_sort_single_element():
-    assert merge_sort([42]) == [42]
+    """Array here is singleton with single element."""
+    arr = [42]
+    assert merge_sort(arr) == sorted(arr), "Array is not sorted"
 
 
 def test_merge_sort_multiple_elements():
-    assert merge_sort([3, 1, 4, 1, 5, 9, 2, 6, 5]) == [
-        1, 1, 2, 3, 4, 5, 5, 6, 9]
+    """Array here is hardcoded."""
+    arr = [3, 1, 4, 1, 5, 9, 2, 6, 5]
+    assert merge_sort(arr) == sorted(arr), "Array is not sorted"
 
 
-if __name__ == "__main__":
-    pytest.main()
+def test_random_array():
+    """Array here is random."""
+    arr = random_array([0] * 20)
+    assert merge_sort(arr) == sorted(arr), "Array is not sorted"
+
+
+def test_sorted_array():
+    """Array here is hardcoded."""
+    arr = [-10000000, -100, -3, -2, -1, 0, 1, 2, 3, 100, 10000000]
+    assert merge_sort(arr) == sorted(arr), "Array is not sorted"
+
+
+def test_only_negatives_array():
+    """Array here is full of negative integers."""
+    arr = [-i for i in range(1, 11)]
+    assert merge_sort(arr) == sorted(arr), "Array is not sorted"
--- tests/test_rand.py
+++ tests/test_rand.py
@@ -1,26 +1,25 @@
-import pytest
+"""testing random array generation."""
 from src.rand import random_array
 
 
 def test_random_array_length():
-    arr = [0, 0, 0, 0, 0]  # Initial array
+    """length test."""
+    arr = [0, 0, 0, 0, 0]
     result = random_array(arr)
-    assert len(result) == 5, "The length of the array should not change."
+    assert len(result) == 5, "The length of the array is changed."
 
 
 def test_random_array_elements_are_int():
-    arr = [0, 0, 0, 0, 0]  # Initial array
+    """type test."""
+    arr = [0, 0, 0, 0, 0]
     result = random_array(arr)
-    for element in result:
-        assert isinstance(element, int), "All elements should be integers."
+    assert all(isinstance(i, int)
+               for i in result), "All elements are not integers."
 
 
 def test_random_array_elements_in_range():
-    arr = [0, 0, 0, 0, 0]  # Initial array
+    """range test."""
+    arr = [0, 0, 0, 0, 0]
     result = random_array(arr)
-    for element in result:
-        assert 1 <= element <= 20, "Elements should be within the range 1 to 20."
-
-
-if __name__ == "__main__":
-    pytest.main()
+    assert all(
+        1 <= i <= 20 for i in result), "Elements should be within the range 1 to 20."
